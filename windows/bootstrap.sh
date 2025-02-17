5a06c0f62d107ddcd2a3c760f295bbfa120269bf
Cezar Garrido Britez <cezar.britez@americanas.io>
2025-02-17 01:39:17 -0300

    chore: fix pip

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -2,7 +2,7 @@ pacman -S --noconfirm \
     mingw-w64-$(uname -m)-gtk3 \
     mingw-w64-$(uname -m)-gcc \
     mingw-w64-$(uname -m)-python-pip \
-    mingw-w64-$(uname -m)-python3-setuptools \
+    mingw-w64-$(uname -m)-python-setuptools \
     mingw-w64-$(uname -m)-python3-gobject \
     mingw-w64-$(uname -m)-poppler \
     mingw-w64-$(uname -m)-libjpeg-turbo \
