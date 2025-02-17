382d95325ba9f7081aace8c89fe27c1a04b1ec76
Cezar Garrido Britez <cezar.britez@americanas.io>
2025-02-16 23:59:55 -0300

    chore: add deps

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -1,1 +1,16 @@
-pacman -S --noconfirm mingw-w64-$(uname -m)-gtk3 mingw-w64-$(uname -m)-python-pip mingw-w64-$(uname -m)-python3-gobject mingw-w64-$(uname -m)-poppler mingw-w64-$(uname -m)-libjpeg-turbo mingw-w64-$(uname -m)-zlib mingw-w64-$(uname -m)-freetype mingw-w64-$(uname -m)-libtiff mingw-w64-$(uname -m)-lcms2 mingw-w64-$(uname -m)-libwebp
+pacman -S --noconfirm \
+    mingw-w64-$(uname -m)-gtk3 \
+    mingw-w64-$(uname -m)-gcc \
+    mingw-w64-$(uname -m)-python-pip \
+    mingw-w64-$(uname -m)-python3-setuptools \
+    mingw-w64-$(uname -m)-python3-gobject \
+    mingw-w64-$(uname -m)-poppler \
+    mingw-w64-$(uname -m)-libjpeg-turbo \
+    mingw-w64-$(uname -m)-zlib \
+    mingw-w64-$(uname -m)-libtiff \
+    mingw-w64-$(uname -m)-freetype \
+    mingw-w64-$(uname -m)-lcms2 \
+    mingw-w64-$(uname -m)-libwebp \
+    mingw-w64-$(uname -m)-openjpeg2 \
+    mingw-w64-$(uname -m)-libimagequant \
+    mingw-w64-$(uname -m)-libraqm
